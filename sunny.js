60d31f2fccaca4efd14e529a51670dfae7bb6472
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-19 20:26:24 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,7 @@
+// dom intro
+
+
+document.getElementById('cite_ref-NYT_1997_09_09_2-0').innerHTML = "<p>no please dont</p>"
+console mn ja k ap kisi b id ko pick kr k usk andr koi b chz target karskty ho like here i targetted para ,dots and commas ka khyl rkho 
+
+only manipulation
